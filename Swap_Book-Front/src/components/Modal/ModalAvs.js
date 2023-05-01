c78b2259a164d9ceb9d34370fe07d6114e2783ba
Silas Matos <silas.progamacao@gmail.com>
2023-05-01 20:59:57 -0300

    product

---
+++ Swap_Book-Front/src/components/Modal/ModalAvs.js
@@ -0,0 +1,69 @@
+import { useState, useEffect } from 'react';
+import { Modal, Button } from 'react-bootstrap';
+import imginfo1 from '../img/imginfo1.jpeg'
+import imginfo2 from '../img/imginfo2.jpeg'
+import imginfo3 from '../img/imginfo3.jpeg'
+import 'bootstrap/dist/css/bootstrap.min.css';
+import '../Modal/Modal.css'
+import { AiOutlineCheck, AiOutlineClose
+} from 'react-icons/ai';
+
+function ModalAvs() {
+  const [showModal, setShowModal] = useState(false);
+
+  useEffect(() => {
+    setShowModal(true);
+  }, []); // a dependência vazia faz com que o useEffect seja executado apenas uma vez, na montagem do componente
+
+  return (
+    <>
+      <Modal show={showModal} onHide={() => setShowModal(false)} size="lg">
+        <Modal.Header closeButton>
+          <Modal.Title>Melhores seus Anuncios</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <p>Para aumentar as chances de sucesso na venda ou troca do seu livro, é recomendado enviar imagens de boa qualidade e em uma angulação adequada. Certifique-se de que a capa e as páginas estejam visíveis e sem sombras ou reflexos, para que os interessados possam ter uma boa noção do estado do livro.
+            <br/>
+            Logo abaixo você poderá visualizar algums exemplos positivos e negativos para o envio da Imagem. Desde já agradecemos a compreensão.
+          </p>
+          <div className="container-info">
+      <div className="image-container">
+        <img
+          src={imginfo1}
+          alt="Imagem 1"
+        />
+        <div className="icon-container">
+          <i className="fas fa-thumbs-up"><AiOutlineCheck/> </i>
+        </div>
+      </div>
+      <div className="image-container">
+        <img
+            src={imginfo2}
+          alt="Imagem 2"
+        />
+        <div className="icon-container">
+          <i className="fas fa-thumbs-down"><AiOutlineClose/></i>
+        </div>
+      </div>
+      <div className="image-container">
+        <img
+           src={imginfo3}
+          alt="Imagem 3"
+        />
+        <div className="icon-container">
+          <i className="fas fa-thumbs-up"><AiOutlineClose/></i>
+        </div>
+      </div>
+    </div>
+        </Modal.Body>
+        <Modal.Footer>
+          <Button variant="primary" onClick={() => setShowModal(false)}>
+            Fechar
+          </Button>
+        </Modal.Footer>
+      </Modal>
+    </>
+  );
+}
+
+export default ModalAvs;
--- Swap_Book-Front/src/components/pages/Dashboard/Dashboard.js
+++ Swap_Book-Front/src/components/pages/Dashboard/Dashboard.js
@@ -7,6 +7,9 @@ import { UserContext } from "../../UseContext/UserContext";
 import { useNavigate } from "react-router-dom";
 import FormData from 'form-data'
 import Footer from "../../Footer/Footer";
+import { FaBook} from 'react-icons/fa';
+import ModalAvs from "../../Modal/ModalAvs";
+
 
 
 
@@ -47,7 +50,115 @@ const Dashboard = () => {
   return (
     <>
       <Navbar2 />
-      <section className="input-section">
+      <ModalAvs/>
+      <div class="container full-df">
+          
+          <section class="panel panel-default">
+          <div class="panel-heading"> 
+          <h3 class="panel-title">Anuncie seus Livros</h3> 
+          </div> 
+          <div class="panel-body">
+            
+          <form action="designer-finish.html" class="form-horizontal" role="form">
+
+          <div class="form-group">
+            <label for="name" class="col-sm-3 control-label">Usuario</label>
+            <div class="col-sm-9">
+              <label class="radio-inline">
+              <input type="radio" name="inlineRadioOptions" id="inlineRadio1" value="option1"></input> Usuario Comum 
+            </label>
+            <label class="radio-inline">
+              <input type="radio" name="inlineRadioOptions" id="inlineRadio2" value="option2"></input> Usuario Pj
+            </label>
+            </div>
+          </div> 
+
+            <div class="form-group">
+              <label for="name" class="col-sm-3 control-label">Título do Livro</label>
+              <div class="col-sm-12">
+                <input type="text" className="form-control" onChange={(e) => setProductName(e.target.value)} placeholder="Nome do Livro" value={productName}></input>
+              </div>
+            </div>
+            <div class="form-group">
+              <label for="name" class="col-sm-3 control-label">Autor</label>
+              <div class="col-sm-12">
+                <input type="text" class="form-control"  placeholder="Autor" value={autorProduct} onChange={(e) => setAutorProduct(e.target.value)}></input>
+              </div>
+            </div> 
+            <div class="form-group">
+              <label for="about" class="col-sm-3 control-label">Sinopse</label>
+              <div class="col-sm-12">
+                <textarea class="form-control" type="text" onChange={(e) => setSinopseProduct(e.target.value)} ></textarea>
+              </div>
+            </div> 
+            
+            <div class="form-group col-sm-12 form-edit-gp">
+              
+              <div class="col-sm-3"> 
+              <label class="control-label small" for="date_start">Ano: </label>
+              <input  class="form-control" type="number" placeholder="Ano" onChange={(e) => setProductYear(e.target.value)}></input>
+              </div>
+            <div class="col-sm-3">   
+              <label class="control-label small" for="date_finish">Preço:</label>
+              <input  class="form-control" type="number"  placeholder="Preço" onChange={(e) => setProductPrice(e.target.value)} ></input>
+              </div>
+              <div className="col-sm-3 ">
+              <select className="form-control" 
+            onChange={(e)=> 
+            setCategoria(e.target.value)}
+            value={categoria}
+             name="Categorias">
+              <option value="Romance"selected> Categória </option>
+              <option value="Ficção" > Ficção</option>
+              <option value="Ação">Ação</option>
+              <option value="Suspense">Suspense</option>
+              <option value="Historia">História</option>
+              <option value="Bibliografia">Bibliografia</option>
+              <option value="Terror">Terror</option>
+              <option value="Fantasia">Fantasia</option>
+              <option value="Infantil">Infantil</option>
+              <option value="Educação">Educação</option>
+            </select>
+              </div>
+            </div> 
+            <div class="form-group">
+              <label for="name" class="col-sm-3 control-label">Insira a Imagem do Livro</label>
+            
+            <div class="col-sm-3">
+            <div className="input-wrapper">
+                  <input type="file" multiple onChange={(e) => setSrc(e.target.files)} />
+                  <span className="btn-preview">Escolher arquivo</span>
+                </div>
+                {src && (
+                  <div className="preview-wrapper">
+                    {Array.from(src).map((file) => (
+                      <img key={file.name} id="img-preview" src={URL.createObjectURL(file)} alt={file.name} />
+                    ))}
+                  </div>
+                )}
+              </div>
+            <button className="btn btn-form-edit" onClick={newProducthandler}>
+          Adicionar Livro
+        </button>
+            </div> 
+     
+          </form>
+            
+          </div>
+        </section>
+</div> 
+
+
+
+      <Footer/>
+    </>
+  );
+};
+
+export default Dashboard;
+
+
+/* <section className="input-section">
   <form className="">
     <h1 className="text-center"> Adicionar um Produto</h1>
     <div className="col-12 container">
@@ -101,10 +212,4 @@ const Dashboard = () => {
     </div>
     </div>
   </form>
-</section>
-<Footer/>
-    </>
-  );
-};
-
-export default Dashboard;
+</section>*/
